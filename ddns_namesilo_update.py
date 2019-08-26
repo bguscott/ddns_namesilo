a17ebb4d50d363307376d7f5eb5fcfc78c914abd
bguscott <32576039+bguscott@users.noreply.github.com>
2019-08-25 22:54:59 -0600

    Update ddns_namesilo_update.py

--- ddns_namesilo_update.py
+++ ddns_namesilo_update.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # coding=utf_8
 #
-# NameSilo DNS Update script 
+# Description:
+# 	NameSilo DNS update with current IP script. Run on a machine on your network and it will auto-update
+#	NameSilo DNS records and subdomains with your current IP. If a subdomain is provided that doesn't exist,
+#	it's created in the process.
+#
 # Must install requests package first
 # python2.7 -m ensurepip
 # python2.7 -m pip install requests
@@ -17,12 +21,15 @@ RECORD_IP_ADDRESS_URL = "https://www.namesilo.com/api/dnsListRecords?version=1&t
 CURRENT_IP_ADDRESS_URL = "http://whatismyip.akamai.com/"
 
 #get current IP address from CURRENT_IP_ADDRESS_URL
-current = requests.get(CURRENT_IP_ADDRESS_URL).content
-print("Current IP address from akamai: %s \n" % current)
+current_ip = requests.get(CURRENT_IP_ADDRESS_URL).content
+print("Current IP address from akamai: %s \n" % current_ip)
 
 #read xml file
 r = requests.get(RECORD_IP_ADDRESS_URL, allow_redirects=True)
 xml = ET.fromstring(r.content)
+
+#list for updating with new A records
+domain_status = dict({status:"unknown" for status in SUB_DOMAIN})
 
 #begin parsing xml for correct host (DOMAIN)
 for record in xml.iter("resource_record"):
@@ -35,23 +42,40 @@ for record in xml.iter("resource_record"):
 	for domain_i in SUB_DOMAIN:
 		if (host == domain_i + "." + DOMAIN):
 			print("Current " + host + " namesilo record IP address is: %s" % value)
-		
+			
 			#if record IP address matches CURRENT_IP_ADDRESS_URL, do nothing
-			if (value == current):
+			if (value == current_ip):
 				print("Current IP address matches namesilo record for %s \n" % host)
+				domain_status[domain_i] = "exists"
 				break
 				
 			#IP addresses don’t match, let’s update it
 			else:
 				print("IP addresses do not match, generating URL to update")
-
-				#place the record_id in the url
-				new_URL = "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=" + API_KEY + "&domain=" + DOMAIN + "&rrid=" + record_id + "&rrhost=" + domain_i + "&rrvalue=" + current + "&rrttl=3600"
+				new_URL = "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=" + API_KEY + "&domain=" + DOMAIN + "&rrid=" + record_id + "&rrhost=" + domain_i + "&rrvalue=" + current_ip + "&rrttl=3600"
 				print(new_URL)
 
 				#send request to URL
 				new = requests.get(new_URL)
 
-				#print the xml reply, this doesn't need to be pretty
+				#print the xml reply
 				print(new.content + "\n")
+				
+				domain_status[domain_i] = "updated"
 				break
+
+#case in which a subdomain currently doesn't exist (value in domain_status for a subdomain is "unknown")
+for domain_i in domain_status:
+	if domain_status[domain_i] == "unknown":
+		print("\"A\" record " + domain_i + "." + DOMAIN + " doesn't exist, generating URL for new A record.")
+		new_URL = "https://www.namesilo.com/api/dnsAddRecord?version=1&type=xml&key=" + API_KEY + "&domain=" + DOMAIN + "&rrtype=A" + "&rrhost=" + domain_i + "&rrvalue=" + current_ip + "&rrttl=3600"
+		print(new_URL + "\n")
+		
+		#send request to URL
+		new = requests.get(new_URL)
+		
+		#print the xml reply
+		print(new.content + "\n")
+			
+		domain_status[domain_i] = "updated"
+		break
